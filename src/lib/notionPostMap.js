5e51c1f58960bc70ff0a8f9bba48c97ac9ed1ca7
picture picture <zz@picturepicture.local>
2025-11-05 19:15:27 -0800

    updated projects

--- src/lib/notionPostMap.js
+++ src/lib/notionPostMap.js
@@ -5,6 +5,7 @@ export const notionPosts = [
       img: "/images/loto_1.jpg",
       title: "ViewShr Industrial Safety App",
       desc: "",
+      tags: ["projects", "product"],
       pageId: "2039b541-6abb-807d-9252-db1b788cb03a",
     },
     {
@@ -12,6 +13,7 @@ export const notionPosts = [
       img: "/images/tableauDash.png",
       title: "Tableau Dashboarding",
       desc: "",
+      tags: ["projects", "data-vis"],
       pageId: "29a9b541-6abb-80de-8870-f8b628cc35e7",
     },
     {
@@ -19,6 +21,7 @@ export const notionPosts = [
       img: "/images/distanceOptimization.png",
       title: "Distance Optimization",
       desc: "",
+      tags: ["projects", "data-vis"],
       pageId: "28f9b541-6abb-80b1-946a-ee75c51d817a",
     },
     {
@@ -26,6 +29,7 @@ export const notionPosts = [
       img: "/images/userProfiles.png",
       title: "User Profiles Infographic",
       desc: "",
+      tags: ["projects", "data-vis"],
       pageId: "2039b541-6abb-80f3-8c82-e84435d46885",
     },
     {
@@ -33,6 +37,7 @@ export const notionPosts = [
       img: "/images/moonViewingChart.jpg",
       title: "Moon Viewing Calendar",
       desc: "",
+      tags: ["projects", "data-vis"],
       pageId: "2039b541-6abb-80ca-944d-d8ca27f6e613",
     },
     {
@@ -40,6 +45,7 @@ export const notionPosts = [
       img: "/images/libraryChairSocks.png",
       title: "Library Quiet Floor Chair Socks",
       desc: "",
+      tags: ["projects", "innovation"],
       pageId: "2869b541-6abb-8078-878d-ef76626777c1",
     },
     {
@@ -47,13 +53,15 @@ export const notionPosts = [
       img: "/images/clogenstocks_technicalIllustration.png",
       title: "Clogenstocks",
       desc: "",
+      tags: ["projects", "product"],
       pageId: "22f9b541-6abb-80f9-a695-d7c149ac9d64",
     },
     {
       slug: "Rothko-Color",
-      img: "/images/rothkoColor.png",
+      img: "/images/rothko-reds.png",
       title: "Mark Rothko's Color",
       desc: "",
+      tags: ["projects", "data-vis"],
       pageId: "2a19b541-6abb-806c-ba6f-e578a35572bd",
     },
   ];
